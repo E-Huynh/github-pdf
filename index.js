fbcacd3b6e471b19a09aacd4e32f0baf3fc6ea94
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-01-26 12:40:51 -0700

    created new repo

---
+++ index.js
@@ -0,0 +1,81 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const generateHTML = require("./generateHTML.js")
+
+//Declare variables
+let finalHTML;
+
+//Call functions
+init();
+
+//functions
+async function init() {
+    try {
+        let { username } = await getUsername();
+        let { color } = await getColor();
+        let profile = await getProfile(username);
+        let { avatar_url, name, company, bio, public_repos, followers, following, html_url, blog, location } = profile;
+        let stars = await getStarred(username);
+        profile.color = color;
+        profile.stars = stars;
+        const html = generateHTML(profile);
+        finalHTML = html;
+        generatePDF();
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+
+
+function getUsername() {
+    const username = inquirer.prompt([{
+        type: "input",
+        message: "Enter a Github username?",
+        name: "username"
+    }]);
+    return username;
+};
+function getColor() {
+    const color = inquirer.prompt([{
+        type: 'list',
+        name: 'color',
+        message: 'What is your favorite color?',
+        choices: ['Green', 'Blue', 'Pink', 'Red']
+    }]);
+    return color;
+};
+function getProfile(username) {
+    return axios.get(`https://api.github.com/users/${username}`).then(res => {
+        return res.data;
+    });
+};
+function getStarred(username) {
+    return axios.get(`https://api.github.com/users/${username}/starred`).then(res => {
+        return res.data.length;
+    });
+};
+async function generatePDF() {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        await page.setContent(`${finalHTML}`);
+        await page.pdf({
+            path: `test.pdf`,
+            pageRanges: "1",
+            format: "A4",
+            printBackground: true
+        });
+
+        console.log("PDF created");
+        await browser.close();
+        process.exit();
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
